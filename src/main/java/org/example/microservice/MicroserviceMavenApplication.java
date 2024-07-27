4169acf2f363d8ffaac298c17e6cc9e9b897043c
timo <1398557+timo-a@users.noreply.github.com>
2024-07-27 18:04:39 +0200

    initial commit

---
+++ src/main/java/org/example/microservice/MicroserviceMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMavenApplication.class, args);
+    }
+
+}
