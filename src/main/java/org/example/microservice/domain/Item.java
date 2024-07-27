784a502b09bbd570b19a90b6a6741b02124a1eea
timo <1398557+timo-a@users.noreply.github.com>
2024-07-27 18:08:23 +0200

    add 2 domain objects

---
+++ src/main/java/org/example/microservice/domain/Item.java
@@ -0,0 +1,4 @@
+package org.example.microservice.domain;
+
+public record Item(String name, Integer value) {
+}
---
+++ src/main/java/org/example/microservice/domain/Reference.java
@@ -0,0 +1,12 @@
+package org.example.microservice.domain;
+
+import lombok.Value;
+
+@Value
+public class Reference {
+
+    String name;
+
+    Integer value;
+}
+
